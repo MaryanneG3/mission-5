2fe7258da0a1ba21b974f7749c656c73d347dc48
Maryanne G <maryanneg@missionreadyhq.com>
2025-01-06 03:41:48 +1300

    added title bar with 2 variants - default and findStation

---
+++ z-fuel-frontend/src/common/titlebar/Titlebar.jsx
@@ -0,0 +1,82 @@
+import styles from "./Titlebar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+
+// to do: create 3 variants of titlebar
+
+// 1. default - h1 title with background color and image
+//            *** create variants for images used in background for default titlebars based on route
+
+// 2. findStation - h3 title with find a station button below it on left section
+
+// 3. shareTank - h2 title with links to apple store and google play store on left section
+//              - image on right section
+
+function Titlebar({ variant, backgroundImage, title }) {
+  const isDefault = () => {
+    if (variant === "default") {
+      return true;
+    }
+  };
+
+  const isFindStation = () => {
+    if (variant === "findStation") {
+      return true;
+    }
+  };
+
+  const isShareTank = () => {
+    if (variant === "shareTank") {
+      return true;
+    }
+  };
+
+  let backgroundClass;
+
+  switch (backgroundImage) {
+    case "welcomeToZ":
+      backgroundClass = styles.welcomeToZ;
+      break;
+    case "find-a-station":
+      backgroundClass = styles.findStation;
+      break;
+    case "shareTank":
+      backgroundClass = styles.shareTank;
+      break;
+    default:
+      backgroundClass = styles.welcomeToZ;
+      break;
+  }
+
+  return (
+    <>
+      {isDefault() && (
+        <div className={`${styles.titlebar} ${backgroundClass}`}>
+          <div className={styles.backgroundColor}>
+            <h1>{title}</h1>
+          </div>
+        </div>
+      )}
+
+      {isFindStation() && (
+        <div className={`${styles.titlebar} ${backgroundClass}`}>
+          <div className={styles.backgroundColor}>
+            <h2>{title}</h2>
+            <div className={styles.findStationButton}>
+              <NavLink to="/find-a-station" className={styles.findStationLink}>
+                <p>Find your Closest Z </p>
+                <FontAwesomeIcon
+                  icon={faCircleArrowRight}
+                  className={styles.icon}
+                />
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Titlebar;
