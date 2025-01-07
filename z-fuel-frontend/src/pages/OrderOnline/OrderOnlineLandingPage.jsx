68390d403fd34b9128d3463a0865c55bc6d279b7
Maryanne G <maryanneg@missionreadyhq.com>
2025-01-08 02:29:28 +1300

    completed order online landing page frontend

--- z-fuel-frontend/src/pages/OrderOnline/OrderOnlineLandingPage.jsx
+++ z-fuel-frontend/src/pages/OrderOnline/OrderOnlineLandingPage.jsx
@@ -3,6 +3,29 @@ import styles from "./OrderOnline.module.css";
 import Titlebar from "../../common/titlebar/Titlebar";
 
 function OrderOnlineLandingPage() {
+  const categories = [
+    {
+      key: 1,
+      category: "Hot drinks",
+      imgSrc: "../../../public/images/orderOnline/cards/Latte.png",
+    },
+    {
+      key: 2,
+      category: "Cold drinks",
+      imgSrc: "../../../public/images/orderOnline/cards/Cold Drinks Large.png",
+    },
+    {
+      key: 3,
+      category: "Food",
+      imgSrc: "../../../public/images/orderOnline/cards/a half eaten pie.png",
+    },
+    {
+      key: 4,
+      category: "Make it a Combo",
+      imgSrc: "../../../public/images/orderOnline/cards/Combo.png",
+    },
+  ];
+
   return (
     <BaseLayout variant="default">
       <Titlebar
@@ -10,7 +33,50 @@ function OrderOnlineLandingPage() {
         backgroundImage="onlineOrder"
         title="Crave it. Order it. Enjoy it."
       />
-      Welcome to the order online landing page
+
+      <div className={styles.informationContainer}>
+        <div className={styles.leftSection}>
+          <h2 className={styles.titles}>Fuel up more than just your car! </h2>
+          <blockquote className={styles.orderOnlineServiceDetails}>
+            <p>
+              Our gas stations offer a delicious range of freshly prepared food
+              and snacks, perfect for busy families and people on the go.{" "}
+            </p>
+            <p>
+              Whether you’re grabbing breakfast, a quick lunch, or a treat for
+              the road, our convenient, high-quality options ensure you stay
+              energized and satisfied wherever your journey takes you.
+            </p>
+            <p>Stop in today and enjoy food that’s ready when you are!</p>
+          </blockquote>
+          <button className={styles.findStationBtn}>
+            Find your nearest Z station
+          </button>
+        </div>
+
+        <div className={styles.rightSection}>
+          <img
+            src="../../../public/images/orderOnline/ZWebsite Food Image.png"
+            className={styles.orderOnlineDefaultImg}
+          />
+        </div>
+      </div>
+
+      <div className={styles.orderContainer}>
+        <h2 className={styles.titles}>
+          Pre-Order Online – Skip the Queue and Save Time!
+        </h2>
+        <div className={styles.categoriesContainer}>
+          {categories.map((category) => (
+            <div key={category.key} className={styles.categoryCard}>
+              <div className={styles.imageControlContainer}>
+                <img src={category.imgSrc} className={styles.categoryImage} />
+              </div>
+              <h3>{category.category}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </BaseLayout>
   );
 }
