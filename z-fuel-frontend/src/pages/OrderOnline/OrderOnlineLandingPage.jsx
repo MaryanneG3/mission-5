37b7a5c87b11d77e2de828cc992b8e9ce4f1e383
Maryanne G <maryanneg@missionreadyhq.com>
2025-01-09 09:43:17 +1300

    completed order online landing and selection page

--- z-fuel-frontend/src/pages/OrderOnline/OrderOnlineLandingPage.jsx
+++ z-fuel-frontend/src/pages/OrderOnline/OrderOnlineLandingPage.jsx
@@ -1,30 +1,51 @@
 import BaseLayout from "../../layouts/baselayout/BaseLayout";
 import styles from "./OrderOnline.module.css";
 import Titlebar from "../../common/titlebar/Titlebar";
+import { useEffect, useState } from "react";
 
 function OrderOnlineLandingPage() {
-  const categories = [
-    {
-      key: 1,
-      category: "Hot drinks",
-      imgSrc: "../../../public/images/orderOnline/cards/Latte.png",
-    },
-    {
-      key: 2,
-      category: "Cold drinks",
-      imgSrc: "../../../public/images/orderOnline/cards/Cold Drinks Large.png",
-    },
-    {
-      key: 3,
-      category: "Food",
-      imgSrc: "../../../public/images/orderOnline/cards/a half eaten pie.png",
-    },
-    {
-      key: 4,
-      category: "Make it a Combo",
-      imgSrc: "../../../public/images/orderOnline/cards/Combo.png",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5001/api/products/all-products"
+        );
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      }
+    };
+
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5001/api/products/categories"
+        );
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories: ", error);
+      }
+    };
+
+    fetchAllProducts();
+    fetchCategories();
+  }, []);
+
+  let filteredProducts = [];
+
+  if (selectedCategory !== null) {
+    filteredProducts = products.filter(
+      (product) => product.category === selectedCategory
+    );
+  } else {
+    filteredProducts = [];
+  }
 
   return (
     <BaseLayout variant="default">
@@ -40,7 +61,7 @@ function OrderOnlineLandingPage() {
           <blockquote className={styles.orderOnlineServiceDetails}>
             <p>
               Our gas stations offer a delicious range of freshly prepared food
-              and snacks, perfect for busy families and people on the go.{" "}
+              and snacks, perfect for busy families and people on the go.
             </p>
             <p>
               Whether you’re grabbing breakfast, a quick lunch, or a treat for
@@ -68,17 +89,142 @@ function OrderOnlineLandingPage() {
         </h2>
         <div className={styles.categoriesContainer}>
           {categories.map((category) => (
-            <div key={category.key} className={styles.categoryCard}>
+            <div
+              key={category.name}
+              className={`${styles.categoryCard} ${
+                selectedCategory === category.name ? styles.activeCategory : ""
+              }`}
+              onClick={() => setSelectedCategory(category.name)}
+            >
               <div className={styles.imageControlContainer}>
-                <img src={category.imgSrc} className={styles.categoryImage} />
+                <img
+                  src={`../../../public/images/orderOnline/cards/${category.imgSrc}`}
+                  className={styles.categoryImage}
+                />
               </div>
-              <h3>{category.category}</h3>
+              <h3>{category.name}</h3>
             </div>
           ))}
         </div>
       </div>
+
+      {selectedCategory && (
+        <div className={styles.productsContainer}>
+          <h3 className={styles.titles}>
+            Select {selectedCategory.toLowerCase()}:
+          </h3>
+          <div className={styles.productsList}>
+            {filteredProducts.map((product) => (
+              <div key={product.name} className={styles.productCard}>
+                <div className={styles.imageControlContainer}>
+                  <img
+                    src={`../../../public/images/orderOnline/${product.imageSource}`}
+                    alt={product.name}
+                    className={styles.productImage}
+                  />
+                </div>
+                <h4>{product.name}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </BaseLayout>
   );
 }
 
 export default OrderOnlineLandingPage;
+
+// const categories = [
+//   {
+//     key: 1,
+//     category: "Hot drinks",
+//     imgSrc: "../../../public/images/orderOnline/cards/Latte.png",
+//   },
+//   {
+//     key: 2,
+//     category: "Cold drinks",
+//     imgSrc: "../../../public/images/orderOnline/cards/Cold Drinks Large.png",
+//   },
+//   {
+//     key: 3,
+//     category: "Food",
+//     imgSrc: "../../../public/images/orderOnline/cards/a half eaten pie.png",
+//   },
+//   {
+//     key: 4,
+//     category: "Make it a Combo",
+//     imgSrc: "../../../public/images/orderOnline/cards/Combo.png",
+//   },
+// ];
+
+// _id
+// 677e97ea04f243b9b78cd8da
+// name
+// "Lemon Ginger Honey"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Lemon-ginger-honey.png"
+// _id
+// 677e97ea04f243b9b78cd8db
+// name
+// "Flat White"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Flat-white.png"
+// _id
+// 677e97ea04f243b9b78cd8dc
+// name
+// "Matcha Latte"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Matcha-latte.png"
+// _id
+// 677e97ea04f243b9b78cd8dd
+// name
+// "Cappuccino"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Cappuccino.png"
+// _id
+// 677e97ea04f243b9b78cd8de
+// name
+// "Hot Chocolate"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Hot-chocolate.png"
+// _id
+// 677e97ea04f243b9b78cd8df
+// name
+// "Latte"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Latte.png"
+// _id
+// 677e97ea04f243b9b78cd8e0
+// name
+// "Chai Latte"
+// category
+// "Hot drinks"
+// inStock
+// true
+// imageSource
+// "Chai-latte.png"
