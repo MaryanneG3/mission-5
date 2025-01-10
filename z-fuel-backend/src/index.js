86fd20805262c5ecb164ae22fdf8c6d6faba6bbe
Caitlin <caitlins@missionreadyhq.com>
2025-01-10 18:40:32 +1300

    updated readme

--- z-fuel-backend/src/index.js
+++ z-fuel-backend/src/index.js
@@ -53,8 +53,4 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
-
 });
-
-
-
