db96b77b5fa74bfa608f37181997e0304294200c
Maryanne G <maryanneg@missionreadyhq.com>
2025-05-09 15:08:03 +1200

    feat-frontend: move service links data to separate module for better organization

---
+++ z-fuel-frontend/src/lib/serviceLinks.js
@@ -0,0 +1,25 @@
+export const serviceLinks = [
+  {
+    serviceTitle: "Sharetank",
+    serviceDescription:
+      "Buy fuel and share with up to 5 friends and family with Sharetank",
+    btnTitle: "Sharetank",
+    btnLink: "/share-tank",
+    imgSrc: "/images/homepage/share-tank.png",
+  },
+  {
+    serviceTitle: "Price comparison",
+    serviceDescription: "Compare fuel prices with nearby stations ",
+    btnTitle: "Price comparison",
+    btnLink: "/compare-prices",
+    imgSrc: "/images/homepage/price-comparison.png",
+  },
+  {
+    serviceTitle: "Order food online",
+    serviceDescription:
+      "Order food and drinks via the app and pick up at the station ",
+    btnTitle: "Order food",
+    btnLink: "/order-online-landing-page",
+    imgSrc: "/images/homepage/order-online.png",
+  },
+];
